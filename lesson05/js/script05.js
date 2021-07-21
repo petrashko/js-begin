b783077b9f7f51ff754cc380a9e582723d23f6d2
petrashko <petrashko@tpu.ru>
2021-07-21 11:20:57 +0700

    Init Lesson05

---
+++ lesson05/js/script05.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// Задание 1
+
+
+//*******************************************************************
+
+const Lesson05 = {
+    run: () => {
+        console.log('Lesson 5');
+        console.log();
+    }
+}
+
+export {
+    Lesson05
+};
