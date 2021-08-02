d476f59e1e10fbcef9736c289da27911883e5b27
petrashko <petrashko@tpu.ru>
2021-08-02 10:49:48 +0700

    Lesson08

--- lesson08/js/script08.js
+++ lesson08/js/script08.js
@@ -1,7 +1,32 @@
 "use strict"
 
 const test = () => {
+    if (!("a" in window)) {
+        var a = 1;
+    }
+    console.log(a);  // => 1
     
+    var b = function a(x) {
+        x && func1(--x);
+    };
+    console.log(a);  // => 1
+    
+    function fuc2(x) {
+        return x * 2;
+    }
+    var func2;
+    console.log(func2); // => Понятия не имею )))
+    
+    function func3(x, y, a) {
+        arguments[2] = 10;  // => 3
+        console.log(a);
+    }
+    func3(1, 2, 3);
+    
+    function func4() {
+        console.log(this);  // => null
+    }
+    func4.call(null);
 }
 
 //*******************************************************************
