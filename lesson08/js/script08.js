2263be41e63d4c7297b0f70c5956d2113709895c
petrashko <petrashko@tpu.ru>
2021-08-02 10:28:43 +0700

    Init Lesson08

---
+++ lesson08/js/script08.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const test = () => {
+    
+}
+
+//*******************************************************************
+
+const Lesson08 = {
+    run: () => {
+        console.log('Lesson 8');
+        console.log();
+        test();
+    }
+}
+
+export {
+    Lesson08
+};
